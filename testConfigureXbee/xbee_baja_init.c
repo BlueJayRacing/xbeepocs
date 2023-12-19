96bf33d246e34dad37cbb9736c5bcf78e7a4c28e
Tanner <tannergladson@gmail.com>
2023-12-19 02:15:24 +0000

    Fixed build issues

--- testConfigureXbee/xbee_baja_init.c
+++ testConfigureXbee/xbee_baja_init.c
@@ -17,7 +17,7 @@
 #include "xbee/wpan.h"
 #include "xbee_baja_config.h"
 #include "serial_port_config.h"
-#include "xbee_init_funcs.h"
+#include "xbee_baja_init.h"
 #include "platform_config.h"
 
 xbee_serial_t _init_serial()
@@ -32,14 +32,55 @@ xbee_serial_t _init_serial()
   return serial;
 }
 
-int _verify_baja_settings(xbee_dev_t *xbee) {
-  // TODO: Use the AT command layer to verify all settings
+int _queue_cmd_requests(xbee_dev_t *xbee, int16_t *request) {
+  // Queue up the AT commands to verify the settings. Return EXIT_FAILURE
+  // if commands could not be queued up
+  
+  // while (1)
+  // {
+  //   xbee_cmd_set_command(request, "AF"); // change the cmd handle to create new command
+  //   xbee_cmd_set_param_bytes(request, "???", length - 6); // Set handle paramteres
+  //   error = xbee_cmd_send(request);
+  //   if (error != 0)
+  //   {
+  //     printf("Error %d setting %s\n", error, "AF");
+  //   }
+  // }
+  
   return 0;
 }
 
-int init_baja_xbee(xbee_dev_t *xbee) {
+int _verify_baja_settings(xbee_dev_t *xbee)
+{
+  /** Verify an XBee has firmware settings that conform
+   *  to the Baja standard. This function will return none
+   *  zero if the XBee does not conform to the standard.
+   */
+  int16_t* request;
+  *request = xbee_cmd_create(xbee, "AF");
+  if (*request >= 0)
+  {
+    xbee_cmd_set_flags(*request, XBEE_CMD_FLAG_QUEUE_CHANGE | XBEE_CMD_FLAG_REUSE_HANDLE);
+    xbee_cmd_send(*request);
+    
+    // Qeueue the commands
+    int err = _uncleanly_verify_settings(xbee, *request);
+    if (err == EXIT_FAILURE) {
+      // Do NOT write the queue of commands
+      xbee_cmd_release_handle(*request);
+      return EXIT_FAILURE; 
+    } 
+  }
+
+  xbee_cmd_execute(xbee, "WR", NULL, 0);
+  xbee_cmd_release_handle(*request);
+  return EXIT_SUCCESS;
+}
+
+int init_baja_xbee(xbee_dev_t *xbee)
+{
   xbee_serial_t serial = _init_serial();
-  
+
   // Dump state to stdout for debug
   int err = xbee_dev_init(xbee, &serial, NULL, NULL);
   if (err)
@@ -48,8 +89,7 @@ int init_baja_xbee(xbee_dev_t *xbee) {
     return EXIT_FAILURE;
   }
   printf("Initialized XBee device abstraction.\n");
-  
-  
+
   // Need to initialize AT layer so we can transmit
   err = xbee_cmd_init_device(xbee);
   if (err)
@@ -57,26 +97,27 @@ int init_baja_xbee(xbee_dev_t *xbee) {
     printf("Error initializing AT layer: %" PRIsFAR "\n", strerror(-err));
     return EXIT_FAILURE;
   }
-  do {
+  do
+  {
     xbee_dev_tick(xbee);
     err = xbee_cmd_query_status(xbee);
   } while (err == -EBUSY);
   if (err)
   {
-    printf( "Error %d waiting for AT init to complete.\n", err);
+    printf("Error %d waiting for AT init to complete.\n", err);
   }
-  
+
   printf("Initialized XBee AT layer\n");
   xbee_dev_dump_settings(xbee, XBEE_DEV_DUMP_FLAG_DEFAULT);
   printf("Veryfing XBee settings conform to Baja standard...\n");
-  
-  err = _verify_baja_settings(xbee);
+
+  err = _safely_verify_baja_settings(xbee);
   if (err)
   {
     printf("Xbee settings were not verified\n");
     return EXIT_FAILURE;
   }
-  
+
   printf("XBee settings conform to Baja standards\n\n");
   return EXIT_SUCCESS;
 }
